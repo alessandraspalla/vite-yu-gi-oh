81707983d9b3497b6ec5349e259853fabb5a92d3
alessandraspalla <alespalla93@icloud.com>
2023-06-05 16:09:13 +0200

    add header component and main in app.vue

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,17 @@ export default {
 
 <template>
   <AppHeader />
+  <main>
+
+  </main>
 </template>
 
 <style lang="scss">
 @use './styles/partials/variables.scss' as *;
 @use './styles/general.scss' as *;
+
+main {
+  background-color: $primary_color;
+  min-height: 90vh;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,6 +4,31 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="logo">
+            <img src="/logo.jpg" alt="logo yu-gi-oh">
+        </div>
+        <h1>
+            Yu-Gi-Oh API
+        </h1>
+    </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    display: flex;
+    align-items: center;
+    height: 50px;
+
+    .logo {
+        width: 80px;
+        margin-inline: 15px;
+        margin-top: 5px;
+    }
+
+    h1 {
+        font-weight: 400;
+    }
+}
+</style>
